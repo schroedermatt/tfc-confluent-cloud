0e93e92c1fa9de461577bbb6a522ff8af5346db3
Matt Schroeder <matthew.schroeder@improving.com>
2022-12-05 15:12:28 -0600

    add variables

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "confluent" {
-  # cloud_api_key    = var.confluent_cloud_api_key    # optionally use CONFLUENT_CLOUD_API_KEY env var
-  # cloud_api_secret = var.confluent_cloud_api_secret # optionally use CONFLUENT_CLOUD_API_SECRET env var
-  # OR
+  cloud_api_key    = var.confluent_cloud_api_key 
+  cloud_api_secret = var.confluent_cloud_api_secret
+
   # export CONFLUENT_CLOUD_API_KEY="<cloud_api_key>"
   # export CONFLUENT_CLOUD_API_SECRET="<cloud_api_secret>"
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,7 @@
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key"
+}
 
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
+}
